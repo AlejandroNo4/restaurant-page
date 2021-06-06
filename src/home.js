9115f585b31a459ad30fa1ad977f7010bdd7e6d1
Alejandro Contreras <kdta15@outlook.com>
2021-06-05 22:01:55 -0500

    Add home.js Add basic divs and styles to the home.js. Add menu.js. Add styles and basic structure nto menu.js. Add images folder and append images to menu page.

---
+++ src/home.js
@@ -0,0 +1,36 @@
+export { homePage };
+
+const homePage = () => {
+  const body = document.body
+  body.classList.add("main-background");
+  body.style.backgroundImage = "url(/dist/images/pub.png)";
+
+  const main = document.getElementById("content");
+  main.appendChild(centerDiv());
+};
+
+const centerDiv = () => {
+  const center = document.createElement("div");
+  const nameContainer = document.createElement("div");
+  const name = document.createElement("h1");
+  const subName = document.createElement("p");
+
+  name.innerText = "Stokehouse";
+  subName.innerText = "Gastro Pub";
+
+  nameContainer.appendChild(name);
+  nameContainer.appendChild(subName);
+
+  name.classList.add("name");
+  nameContainer.classList.add("restaurant-name");
+  const testimonial = document.createElement("p");
+
+  testimonial.innerText =
+    '"The best place in town with the best meat and service." - Gastro Magazine';
+  testimonial.classList.add("quote");
+  center.appendChild(nameContainer);
+  center.appendChild(testimonial);
+  center.classList.add("main-center");
+
+  return center;
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,76 @@
+import { homePage } from "/src/home";
+import { menuPage } from "/src/menu";
 
-console.log("Thiswoked!")
+const navBar = () => {
+  const nav = document.createElement("nav");
+  const listOfElements = document.createElement("ul");
+  const home = document.createElement("li");
+  const contact = document.createElement("li");
+  const menu = document.createElement("li");
+
+  home.innerText = "Home";
+  contact.innerText = "Contact";
+  menu.innerText = "Menu";
+
+  home.classList.add("nav-item");
+  contact.classList.add("nav-item");
+  menu.classList.add("nav-item");
+
+  listOfElements.appendChild(home);
+  listOfElements.appendChild(menu);
+  listOfElements.appendChild(contact);
+
+  home.addEventListener('click', ()=>{
+    homePage()
+  })
+  menu.addEventListener('click', () =>{
+    menuPage()
+  })
+
+  listOfElements.classList.add("d-flex", "navbar");
+
+  nav.appendChild(listOfElements);
+
+  return nav;
+};
+
+const footer = () => {
+  const footer = document.createElement("footer");
+
+  const listOfFooter = document.createElement("ul");
+  const canva = document.createElement("li");
+  const github = document.createElement("li");
+  const icons = document.createElement("li");
+
+  const gitIcon = document.createElement("img");
+  const gitLegend = document.createElement("span");
+  gitIcon.src = "/dist/images/icons/github.svg";
+  gitIcon.classList.add("github-logo");
+  gitLegend.innerText = "Github";
+  github.appendChild(gitIcon);
+  github.appendChild(gitLegend);
+
+  canva.innerText = "Images: Canva.com";
+  icons.innerText = "Icons: Icons8.com";
+
+  canva.classList.add("footer-item");
+  icons.classList.add("footer-item");
+  github.classList.add("footer-item", "d-flex");
+
+  listOfFooter.appendChild(canva);
+  listOfFooter.appendChild(icons);
+  listOfFooter.appendChild(github);
+
+  listOfFooter.classList.add("d-flex", "footer");
+
+  footer.appendChild(listOfFooter);
+
+  return footer;
+};
+
+const main = document.getElementById("content");
+main.appendChild(navBar());
+main.appendChild(footer());
+
+homePage()
+
